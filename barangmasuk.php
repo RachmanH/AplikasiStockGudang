78cad5bedafae7979c30f067e0028142b3aea29c
RachmanH <147359506+RachmanH@users.noreply.github.com>
2024-07-27 13:10:34 +0700

    barangmasuk.php

--- barangmasuk.php
+++ barangmasuk.php
@@ -235,7 +235,7 @@ include './components/head.php';
       
         <!-- Modal Header -->
         <div class="modal-header">
-          <h4 class="modal-title">Modal Heading</h4>
+          <h4 class="modal-title">Tambah Barang Masuk</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         
